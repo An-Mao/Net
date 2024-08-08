4578a593ed4deb2bb4d08306afe723e3ce1fe60b
AnMao <lxxl@live.cn>
2024-08-08 11:09:00 +0800

    ver 24.08.08.01

---
+++ src/main/java/am/mc/net/Net.java
@@ -0,0 +1,10 @@
+package am.mc.net;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Net.MOD_ID)
+public class Net {
+    public static final String MOD_ID = "net";
+    public Net() {
+    }
+}
